42e34a3e929da5112a2bf8059d3ff23353c6cdb6
Esteban Portillo <esteban.07.elias@gmail.com>
2023-01-03 16:19:27 -0600

    Playground: Manejo de Clases

--- main.js
+++ main.js
@@ -88,3 +88,71 @@ const miguelito2 = new Student({
     email: 'miguelito@miguelito.com',
     instagram: 'miguelito_feliz',
 });
+
+/*
+Dadas la siguientes clases:
+
+Clase Banda con las propiedades:
+
+nombre - string
+generos - Array
+integrantes - Array
+Crea una clase llamada "Integrante" con las propiedades:
+
+nombre - string
+instrumento - string
+
+Escribe la lógica para agregar integrantes a la clase Banda
+en la función agregarIntegrante. En esta función no debe dejar
+agregar más de un baterista, es decir hay que validar que solo
+se pueda agregar un integrante con instrumento "Bateria".
+*/
+
+// SOLUCIÓN:
+
+class Banda {
+    constructor({ nombre, generos = [] }) {
+        this.nombre = nombre;
+        this.generos = generos;
+        this.integrantes = [];
+    }
+
+    agregarIntegrante(integranteNuevo) {
+        const tenemosBaterista = this.integrantes.some(
+            (integrante) => integrante.instrumento === 'Bateria'
+        );
+
+        const nuevoIntegranteBaterista =
+            integranteNuevo.instrumento === 'Bateria';
+
+        // Validar si ya existe un baterista
+        if (tenemosBaterista && nuevoIntegranteBaterista) {
+            return;
+        }
+
+        this.integrantes.push(integranteNuevo);
+    }
+}
+
+//Crear clase Integrante
+class Integrante {
+    constructor({ nombre, instrumento }) {
+        this.nombre = nombre;
+        this.instrumento = instrumento;
+    }
+}
+
+const data = {
+    nombre: 'Los Jacks',
+    generos: ['rock', 'pop', 'post-punk'],
+    integrantes: [],
+};
+const banda = new Banda(data);
+
+banda.agregarIntegrante(
+    new Integrante({ nombre: 'Elijah', instrumento: 'Guitarra' })
+);
+
+banda.agregarIntegrante(
+    new Integrante({ nombre: 'Joshua', instrumento: 'Bateria' })
+);
